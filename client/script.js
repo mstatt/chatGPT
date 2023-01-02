2e2b93e254a03d9d6edefbffb0efecf4fd33ed16
Michael Stattelman <mstatt@users.noreply.github.com>
2023-01-03 03:43:26 +0400

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('<your deployment server>', {
+    const response = await fetch('https://chatgpt-fai.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
